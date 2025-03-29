1aa58cda68fd0c289c5cfe8f7a41347a78eec988
hemalatha0303 <hemalathareddy.7254@gmail.com>
2025-03-29 11:03:37 +0530

    Initial commit: Added ReactJS project

---
+++ fetch-axios-app/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [fact, setFact] = useState("");
+  const fetchClick = () => {
+    fetchFact()
+  }
+  const axiosClick = () => {
+    fetchFact1()
+   }
+  const fetchFact = () => {
+    fetch("https://catfact.ninja/fact").then((response) => response.json()).then((data) => setFact(data.fact));
+  }
+  const fetchFact1 = () => {
+    axios.get("https://catfact.ninja/fact").then((response) => {setFact(response.data.fact)});
+  }
+  useEffect(() => {fetchFact()}, []);
+  return (
+    <div className="App">
+    <h2>FETCH AND AXIOS</h2>
+    <button onClick= {() => fetchClick()}>using fetch Getting Cat fact </button>
+    <button onClick= {() => axiosClick()}>using axios Getting cat fact </button>
+    <p>{fact}</p>
+    </div>
+  );
+}
+export default App;
+
